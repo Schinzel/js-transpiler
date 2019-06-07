5bea99f0ff380e8e331b7eaba4e38f4127acd413
Jörgen Andersson <jorgen.andersson@casaferre.se>
2019-06-07 13:47:32 +0200

    Handle instant with tests

---
+++ src/test/kotlin/io/schinzel/jstranspiler/transpiler/JsContructorInitTest.kt
@@ -0,0 +1,46 @@
+package io.schinzel.jstranspiler.transpiler
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.time.Instant
+
+
+class JsContructorInitTest {
+
+    private data class IntegerClass(val value: Int)
+    private data class FloatClass(val value: Float)
+    private data class DoubleClass(val value: Double)
+    private data class StringClass(val value: String)
+    private data class BooleanClass(val value: Boolean)
+    private data class InstantClass(val value: Instant)
+
+    @Test
+    fun toJavaScript_intValueEndsWith_parseInt() {
+        assertThat(JsConstructorInit(IntegerClass::value).toJavaScript()).endsWith("this.value = parseInt(json.value);")
+    }
+
+    @Test
+    fun toJavaScript_floatValueEndWith_parseFloat() {
+        assertThat(JsConstructorInit(FloatClass::value).toJavaScript()).endsWith("this.value = parseFloat(json.value);")
+    }
+
+    @Test
+    fun toJavaScript_dobleValueEndsWith_parseFloat() {
+        assertThat(JsConstructorInit(DoubleClass::value).toJavaScript()).endsWith("this.value = parseFloat(json.value);")
+    }
+
+    @Test
+    fun toJavaScript_stringValueEndsWith_assignValue() {
+        assertThat(JsConstructorInit(StringClass::value).toJavaScript()).endsWith("this.value = json.value;")
+    }
+
+    @Test
+    fun toJavaScript_booleanValueEndsWith_assignValue() {
+        assertThat(JsConstructorInit(BooleanClass::value).toJavaScript()).endsWith("this.value = json.value;")
+    }
+
+    @Test
+    fun toJavaScript_instantValueEndsWith_newDate() {
+        assertThat(JsConstructorInit(InstantClass::value).toJavaScript()).endsWith("this.value = new Date(json.value);")
+    }
+}
