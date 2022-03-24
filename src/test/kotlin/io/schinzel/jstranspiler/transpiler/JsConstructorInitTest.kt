2e86953c9d44a694abdc644c5892a9b31262f699
Henrik Schinzel <henrik@schinzel.io>
2022-03-24 19:37:00 +0100

    Added test

--- src/test/kotlin/io/schinzel/jstranspiler/transpiler/JsConstructorInitTest.kt
+++ src/test/kotlin/io/schinzel/jstranspiler/transpiler/JsConstructorInitTest.kt
@@ -3,6 +3,7 @@ package io.schinzel.jstranspiler.transpiler
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.time.Instant
+import java.time.LocalDate
 
 
 class JsConstructorInitTest {
@@ -13,9 +14,10 @@ class JsConstructorInitTest {
     private data class StringClass(val value: String)
     private data class BooleanClass(val value: Boolean)
     private data class InstantClass(val value: Instant)
+    private data class LocalDateClass(val value: LocalDate)
 
     @Test
-    fun toJavaScript_intValueEndsWith_parseInt() {
+    fun toJavaScript_int_parseInt() {
         assertThat(JsConstructorInit(IntegerClass::value).toJavaScript()).endsWith("this.value = parseInt(json.value);")
     }
 
@@ -25,22 +27,27 @@ class JsConstructorInitTest {
     }
 
     @Test
-    fun toJavaScript_doubleValueEndsWith_parseFloat() {
+    fun toJavaScript_double_parseFloat() {
         assertThat(JsConstructorInit(DoubleClass::value).toJavaScript()).endsWith("this.value = parseFloat(json.value);")
     }
 
     @Test
-    fun toJavaScript_stringValueEndsWith_assignValue() {
+    fun toJavaScript_string_assignValue() {
         assertThat(JsConstructorInit(StringClass::value).toJavaScript()).endsWith("this.value = json.value;")
     }
 
     @Test
-    fun toJavaScript_booleanValueEndsWith_assignValue() {
+    fun toJavaScript_boolean_assignValue() {
         assertThat(JsConstructorInit(BooleanClass::value).toJavaScript()).endsWith("this.value = json.value;")
     }
 
     @Test
-    fun toJavaScript_instantValueEndsWith_newDate() {
+    fun toJavaScript_instant_newDate() {
         assertThat(JsConstructorInit(InstantClass::value).toJavaScript()).endsWith("this.value = new Date(json.value);")
+    }
+
+    @Test
+    fun toJavaScript_localDate_newDate() {
+        assertThat(JsConstructorInit(LocalDateClass::value).toJavaScript()).endsWith("this.value = json.value;")
     }
 }
